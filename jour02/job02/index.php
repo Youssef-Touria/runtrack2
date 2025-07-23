5db9e7c39d68c0b1196d861a7b9a0c9ec57aaf20
Youssef-Touria <touria.youssef@laplateforme.io>
2025-07-23 02:43:09 +0200

    Ajout du job02 : affichage Hello LaPlateforme avec commentaires

---
+++ jour02/job02/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Ceci est un commentaire sur une seule ligne
+echo "Hello LaPlateforme!";
+
+/*
+ Ceci est un commentaire
+ sur plusieurs lignes (bloc)
+*/
+echo "Hello LaPlateforme!";
+
+?>
