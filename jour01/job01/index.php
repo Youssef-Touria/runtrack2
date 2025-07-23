38809c46d5371d8ad9943f45b795c971d2734e3f
Youssef-Touria <touria.youssef@laplateforme.io>
2025-07-23 02:24:08 +0200

    Ajout du fichier index.php pour job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Partie 1
+$str = "LaPlateforme";
+echo $str . "\n";
+
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3 . "\n";
+
+// Partie 2
+$val = 6;
+echo $val . "\n";
+
+$val += 4;
+echo $val . "\n";
+
+// Partie 3
+$myBool = true;
+echo $myBool . "\n";
+
+$myBool = false;
+echo $myBool . "\n";
+
+?>
