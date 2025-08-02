801de380661b75f2a58140d97114349df97f5dfb
Youssef-Touria <touria.youssef@laplateforme.io>
2025-08-02 08:03:24 +0200

    t

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -2,11 +2,33 @@
 // Hauteur du triangle
 $hauteur = 5;
 
-// Affiche le triangle avec des *
 for ($i = 1; $i <= $hauteur; $i++) {
-    for ($j = 1; $j <= $i; $j++) {
-        echo "*";
+    // Espaces pour centrer
+    for ($j = 1; $j <= $hauteur - $i; $j++) {
+        echo "&nbsp;&nbsp;";
     }
-    echo "<br />";
+
+    // Première ligne (pointe)
+    if ($i == 1) {
+        echo "^";
+    }
+    // Dernière ligne (base)
+    elseif ($i == $hauteur) {
+        echo "/";
+        for ($k = 1; $k <= ($i - 1) * 2 - 1; $k++) {
+            echo "-";
+        }
+        echo "\\";
+    }
+    // Lignes intermédiaires (côtés en tirets)
+    else {
+        echo "/";
+        for ($k = 1; $k <= ($i - 1) * 2 - 1; $k++) {
+            echo "&nbsp;&nbsp;";
+        }
+        echo "\\";
+    }
+
+    echo "<br>";
 }
 ?>
