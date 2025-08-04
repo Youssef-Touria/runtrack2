9eb48c315359f89aa9020c9bbc78b77434b83e4d
Youssef-Touria <touria.youssef@laplateforme.io>
2025-08-04 23:16:34 +0200

    t

---
+++ jour07/job03/job03.php
@@ -0,0 +1,13 @@
+<?php
+// Déclaration de la fonction
+function getHello() {
+    return "Hello LaPlateforme!";
+}
+
+// Appel de la fonction et récupération de sa valeur
+$message = getHello();
+
+// Affichage du message
+echo $message;
+?>
+
