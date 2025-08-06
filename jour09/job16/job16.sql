d88a776972a74104eb4c18549f79559b0412b224
Youssef-Touria <touria.youssef@laplateforme.io>
2025-08-06 11:05:30 +0200

    t

---
+++ jour09/job16/job16.sql
@@ -0,0 +1,7 @@
+SELECT etage.nom AS etage, 
+       salles.nom AS 'Biggest Room', 
+       salles.capacite
+FROM salles
+INNER JOIN etage ON salles.id_etage = etage.id
+ORDER BY salles.capacite DESC
+LIMIT 1;
