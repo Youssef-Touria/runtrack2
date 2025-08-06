d6da83c5c0190b0bb535a3325565dd0184598486
Youssef-Touria <touria.youssef@laplateforme.io>
2025-08-06 16:23:35 +0200

    t

--- jour08/job04/index.php
+++ jour08/job04/index.php
@@ -5,15 +5,18 @@ $cookie_name = "prenom";
 // Si on clique sur "connexion"
 if (isset($_POST['connexion']) && !empty(trim($_POST['prenom']))) {
     $prenom = htmlspecialchars(trim($_POST['prenom']));
-    // Cookie valable 1 heure
-    setcookie($cookie_name, $prenom, time() + 3600);
-    $_COOKIE[$cookie_name] = $prenom; // Pour l'utiliser immédiatement
+
+    // Création du cookie (valable 1h)
+    setcookie($cookie_name, $prenom, time() + 3600, "/");
+
+    // Permet d'utiliser immédiatement la valeur sans recharger
+    $_COOKIE[$cookie_name] = $prenom;
 }
 
 // Si on clique sur "déconnexion"
 if (isset($_POST['deco'])) {
-    // Supprimer le cookie
-    setcookie($cookie_name, "", time() - 3600);
+    // Suppression du cookie
+    setcookie($cookie_name, "", time() - 3600, "/");
     unset($_COOKIE[$cookie_name]);
 }
 ?>
@@ -26,4 +29,20 @@ if (isset($_POST['deco'])) {
 </head>
 <body>
 <div class="container">
-    <?php if [!isset$_COOKIE[$cookie_name]_]()_
+    <?php if (!isset($_COOKIE[$cookie_name])): ?>
+        <!-- Formulaire de connexion -->
+        <h2>Connexion</h2>
+        <form method="post">
+            <input type="text" name="prenom" placeholder="Entrez votre prénom">
+            <button type="submit" name="connexion" class="btn-primary">Connexion</button>
+        </form>
+    <?php else: ?>
+        <!-- Message de bienvenue -->
+        <h2>Bonjour <?= htmlspecialchars($_COOKIE[$cookie_name]) ?> !</h2>
+        <form method="post">
+            <button type="submit" name="deco" class="btn-reset">Déconnexion</button>
+        </form>
+    <?php endif; ?>
+</div>
+</body>
+</html>
